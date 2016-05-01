2dfba8fcc7ac510794413520751cbf2fc92300af
Foz <fozolo@gmail.com>
2016-05-01 19:18:26 -0400

    Fix shared doors on_rightclick behavior
    The doors API doesn't provide for custom on_rightclick
    functions. Previously I changed the doors API, but its
    better to just override it here so this mod will work
    with upstream version of minetest_game.

--- shared_door.lua
+++ shared_door.lua
@@ -1,15 +1,24 @@
-doors.register("landrush_shared_door", {
+doors.register("landrush:shared_door", {
 	description = "Shared Door",
 	inventory_image = "shared_door_inv.png",
 	groups = {snappy=1,bendy=2,cracky=1,melty=2,level=2,door=1},
 	tiles = {{ name = "shared_door.png", backface_culling = true }},
-	on_rightclick = function(pos, node, clicker)
+})
+
+local types = {
+	"landrush:shared_door_a",
+	"landrush:shared_door_b",
+}
+
+for _,v in ipairs(types) do
+	local def = minetest.registered_nodes[v]
+	def.on_rightclick = function(pos, node, clicker)
 		if ( landrush.can_interact(pos,clicker:get_player_name()) ) then
 			local door = doors.get(pos)
 			door:toggle()
 		end
 	end
-})
+end
 
 minetest.register_craft({
 	output = 'doors:landrush_shared_door',
@@ -19,4 +28,3 @@ minetest.register_craft({
 		{'default:steel_ingot','default:steel_ingot',''}
 	}
 })
-
